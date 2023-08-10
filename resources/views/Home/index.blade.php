7a697621ab5578c8dab2e25a856b692a4fd61dcb
Kesya Febriana Manampiring <kesya.febriana@student.umn.ac.id>
2023-08-10 14:03:44 +0700

    first commit

---
+++ resources/views/Home/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>UMN Radio</title>
+
+    </head>
+    <body class="composer">
+        <div class="container">
+            <h1>Shalom!</h1>
+        </div>
+    </body>
+</html>
